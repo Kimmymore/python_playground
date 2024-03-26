bea0aa521072b4b51031f165124da3b565870ed8
Kim <54950911+Kimmymore@users.noreply.github.com>
2024-03-26 12:44:29 +0100

    breeds library

---
+++ PADAD/BreedsLibrary.py
@@ -0,0 +1,35 @@
+class DogBreed:
+  def __init__(self, name, size, temperament, origin):
+    self.name = name
+    self.size = size
+    self.temperament = temperament
+    self.origin = origin
+
+# Example usage:
+golden_retriever = DogBreed("Golden Retriever", "Large", "Intelligent, Friendly, Devoted", "Scotland")
+labrador_retriever = DogBreed("Labrador Retriever", "Large", "Outgoing, Even Tempered, Gentle", "Canada")
+labradoodle = DogBreed("Labradoodle", "Medium", "Intelligent, Friendly, Outgoing", "Australia")
+australian_shepherd = DogBreed("Australian Shepherd", "Medium", "Intelligent, Energetic, Work-Oriented", "United States")
+poodle = DogBreed("Poodle", "Medium", "Intelligent, Active, Alert", "Germany")
+german_shepherd = DogBreed("German Shepherd", "Large", "Courageous, Confident, Smart", "Germany")
+bulldog = DogBreed("Bulldog", "Medium", "Docile, Willful, Friendly", "England")
+beagle = DogBreed("Beagle", "Small", "Curious, Merry, Friendly", "England")
+pug = DogBreed("Pug", "Small", "Charming, Mischievous, Loving", "China")
+dachshund = DogBreed("Dachshund", "Small", "Clever, Stubborn, Devoted", "Germany")
+toy_poodle = DogBreed("Toy Poodle", "Small", "Intelligent, Alert, Active", "France")
+rottweiler = DogBreed("Rottweiler", "Large", "Good-natured, Fearless, Courageous", "Germany")
+siberian_husky = DogBreed("Siberian Husky", "Large", "Outgoing, Alert, Gentle", "Russia")
+pomeranian = DogBreed("Pomeranian", "Small", "Extroverted, Friendly, Intelligent", "Germany")
+chihuahua = DogBreed("Chihuahua", "Small", "Graceful, Charming, Sassy", "Mexico")
+shih_tzu = DogBreed("Shih Tzu", "Small", "Affectionate, Outgoing, Playful", "China")
+boxer = DogBreed("Boxer", "Large", "Playful, Energetic, Friendly", "Germany")
+doberman = DogBreed("Doberman", "Large", "Fearless, Energetic, Alert", "Germany")
+great_dane = DogBreed("Great Dane", "Extra Large", "Friendly, Patient, Dependable", "Germany")
+pembroke_welsh_corgi = DogBreed("Pembroke Welsh Corgi", "Small", "Smart, Alert, Affectionate", "Wales")
+saint_bernard = DogBreed("Saint Bernard", "Extra Large", "Friendly, Gentle, Watchful", "Switzerland")
+shetland_sheepdog = DogBreed("Shetland Sheepdog", "Medium", "Affectionate, Loyal, Intelligent", "Scotland")
+papillon = DogBreed("Papillon", "Small", "Friendly, Alert, Happy", "France")
+maltese = DogBreed("Maltese", "Small", "Gentle, Playful, Charming", "Italy")
+border_collie = DogBreed("Border Collie", "Medium", "Intelligent, Energetic, Alert", "United Kingdom")
+boston_terrier = DogBreed("Boston Terrier", "Small", "Friendly, Bright, Amusing", "United States")
+king_charles_spaniel = DogBreed("King Charles Spaniel", "Small", "Affectionate, Gentle, Graceful", "United Kingdom")
---
+++ PADAD/README.md
@@ -0,0 +1,22 @@
+<header>
+
+## 🐶 Pet a dog a day!
+
+</header>
+
+### About this app
+This app challenges you to go outside and pet dogs! <br>
+With this app you can keep track of all the dogs you pet 🐶 <br>
+<br>
+**Make sure you ask the person who is with the dog permission first and always approach the dog gently and with care! All good? Pet that dog!** <br>
+<br>
+💫 Earn a streak for petting different dogs, multiple days in a row. <br>
+📝 Add each dog you pet to your Cachorrinhos collection and collect all the breeds. <br>
+💜 To complete your Cachorrinhos collection you can add a picture of the dog, a great way to look back on all the cuties you've met. <br>
+🐕 Find more information about different dog breeds in the dog breeds library. <br>
+🤗 Be kind! If a dog is nervous or doesn't like to be petted that's okay! The dog's wellbeing always comes first! You can still add the dog to your Cachorrinhos collection. <br>
+<br>
+**Because petting a cute dog makes every day better!**
+
+
+
